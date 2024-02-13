df2e3851317b95cd9605ffd73598ae6a0184c989
Holandaise <adamlmontgomery@gmail.com>
2024-02-13 18:34:04 -0500

    trying to stop the autosave files

--- init.el
+++ init.el
@@ -1,20 +1,3 @@
-;; Adam's custom emacs config I liked doom, but it's just too much overhead; I
-;; mean, the kitchen sink was included It also has so many packages that I don't
-;; know what they all do. For me, it's better to just build a config from
-;; scratch, learn a bit about elisp, and the emacs environment along the way.
-
-;; I also feel like the all-in-one solutions like doom, astrovim, and the like
-;; go against the spirit of using extensible editors. They're extensibile for
-;; YOU, when YOU'RE ready, or find it necessary to make customizations. Another
-;; problem I had with the ready-made solutions is, how do I know where the
-;; underlying tool stops and the customization begins?
-
-;; It's kinda like REACT for front-end development. There's a tool to do the
-;; job, Javascript, but people have piled cruft on top of it to make building a
-;; website "easier". The problem is, now devs have to learn the language and the
-;; framework(s).
-
-
 ;; Global variables
 (setq gfont-height 130
       org-directory "~/org"
@@ -27,7 +10,6 @@
 ;; package.el
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
 
 (package-initialize)
 (package-refresh-contents)
@@ -51,8 +33,8 @@
   (package-install 'doom-themes))
 (require 'doom-themes)
 ;; Modus theme customization
-;; (setq modus-themes-region '(bg-only))
-;; (setq modus-themes-fringes 'intense)
+(setq modus-themes-region '(bg-only))
+(setq modus-themes-fringes 'intense)
 (load-theme 'modus-vivendi)
 
 ;; set font
@@ -70,6 +52,9 @@
 (global-hl-line-mode 1) 
 
 ;; No backup files DAMMIT!
+(setq backup-directory-alist
+      '((".*" . ,temporary-file-directory)))
+
 (setq make-backup-files nil)
 (setq backup-inhibited t)
 
@@ -143,7 +128,6 @@
 	 "* TODO %?\nDEADLINE: <%(end-of-week)>")))
 
 ;; keybinds
-;;(load-file "am-keybinds.el")
 (require 'am-keybinds)
 
 ;; For org-capture template
@@ -158,7 +142,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "2f8eadc12bf60b581674a41ddc319a40ed373dd4a7c577933acaff15d2bf7cc6" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "b54376ec363568656d54578d28b95382854f62b74c32077821fdfd604268616a" "3fe1ebb870cc8a28e69763dde7b08c0f6b7e71cc310ffc3394622e5df6e4f0da" "a9abd706a4183711ffcca0d6da3808ec0f59be0e8336868669dc3b10381afb6f" "8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" "b99e334a4019a2caa71e1d6445fc346c6f074a05fcbb989800ecbe54474ae1b0" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "49acd691c89118c0768c4fb9a333af33e3d2dca48e6f79787478757071d64e68" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
+   '("631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "2f8eadc12bf60b581674a41ddc319a40ed373dd4a7c577933acaff15d2bf7cc6" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "1aa4243143f6c9f2a51ff173221f4fd23a1719f4194df6cef8878e75d349613d" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "b54376ec363568656d54578d28b95382854f62b74c32077821fdfd604268616a" "3fe1ebb870cc8a28e69763dde7b08c0f6b7e71cc310ffc3394622e5df6e4f0da" "a9abd706a4183711ffcca0d6da3808ec0f59be0e8336868669dc3b10381afb6f" "8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" "b99e334a4019a2caa71e1d6445fc346c6f074a05fcbb989800ecbe54474ae1b0" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "49acd691c89118c0768c4fb9a333af33e3d2dca48e6f79787478757071d64e68" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
  '(package-selected-packages
    '(projectile marginalia consult evil-collection doom-themes corfu-mode rust-mode zig-mode lsp-mode which-key vertico)))
 (custom-set-faces
