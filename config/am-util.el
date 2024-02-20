19b4ad797fae26c7e9a531956952a96d6b20fc9b
Holandaise <adamlmontgomery@gmail.com>
2024-02-20 18:40:02 -0500

    Included zettelkasten functionality

--- config/am-util.el
+++ config/am-util.el
@@ -31,3 +31,20 @@
   (format-time-string "%F %a"
 		      (time-add (current-time)
 				(seconds-to-time (* (- 7 (nth 6 (decode-time))) 86400)))))
+
+(defcustom zettel-directory "~/zet" "The directory where your zettelkasten are to be stored")
+
+(add-to-list 'org-agenda-files zettel-directory)
+
+(defun zettel ()
+  "open this week's zet if not exists"
+  (interactive)
+  (unless (file-exists-p zettel-directory)
+    (make-directory zettel-directory))
+  (org-open-file (zet--build-filepath)))
+
+(defun zet--build-filepath ()
+  "return the filename with the current week number"
+  (file-name-concat
+   (file-name-as-directory zettel-directory)
+   (format-time-string "week-%V.org")))
